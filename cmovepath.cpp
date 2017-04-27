5bbd7414b9789d15bba485358709206707c2ed28
luogang <luogang@acoinfo.com>
2017-04-27 18:15:26 +0800

    Add track Class;

---
+++ cmovepath.cpp
@@ -0,0 +1,358 @@
+﻿#include "cmovepath.h"
+#include <QtMath>
+#include <QDebug>
+
+CShapeCircle::CShapeCircle(const QPoint &centre, qreal radius)
+{
+    moCentre = centre;
+    mfRadius = radius;
+}
+
+CShapeCircle::CShapeCircle(const QPoint &centre, const QPoint &point)
+{
+    moCentre = centre;
+    mfRadius = CMovePath::lengthToPoint(centre, point);
+}
+
+QList<QPoint> CShapeCircle::getPoint(qreal x, bool isX)
+{
+    qreal other = 0;
+    int length = 0;
+    int ab = 0;
+    if (isX)
+    {
+        length = x - moCentre.x();
+        ab = moCentre.y();
+    }
+    else
+    {
+        length = x - moCentre.y();
+        ab = moCentre.x();
+    }
+    other = qSqrt(mfRadius * mfRadius - length * length);
+
+    if (isX)
+    {
+        return QList<QPoint>() << QPoint(x, other + ab) << QPoint(x, -other + ab);
+    }
+    else
+    {
+        return QList<QPoint>() << QPoint(other + ab, x) << QPoint(-other + ab, x);
+    }
+}
+
+CMovePath::CMovePath(CMovePath::TYPE type)
+{
+    this->meType = type;
+    this->mfStep = 0;
+}
+
+CMovePath::CMovePath(const QPoint &start, const QPoint &end, CMovePath::TYPE type)
+{
+    QtoCPoint(this->moStartPoint, start);
+    switch (type)
+    {
+    case LINE:
+        QtoCPoint(this->moEnd, end);
+        break;
+    case RECT:
+        QtoCPoint(this->moRect, end);
+        break;
+    case ARC:
+        QtoCPoint(this->moCentre, end);
+        break;
+    default:
+        QtoCPoint(this->moEnd, end);
+        break;
+    }
+    this->meType = type;
+    this->mfStep = 1;
+}
+
+CMovePath::CMovePath(const QPoint &start, const QPoint &leftCentre, const QPoint &rightCentre)
+{
+    QtoCPoint(this->moStartPoint, start);
+    QtoCPoint(this->moLeftCentre, leftCentre);
+    QtoCPoint(this->moRightCentre, rightCentre);
+    this->meType = ELLIPSE;
+    this->mfStep = 1;
+}
+
+void CMovePath::QtoCPoint(CPoint &p1, const QPoint &p2)
+{
+    p1.x = p2.x();
+    p1.y = p2.y();
+}
+
+void CMovePath::setType(CMovePath::TYPE type)
+{
+    this->meType = type;
+}
+
+void CMovePath::setSetp(qreal setp)
+{
+    this->mfStep = setp;
+}
+
+void CMovePath::setLine(const QPoint &start, const QPoint &end)
+{
+    QtoCPoint(this->moStartPoint, start);
+    QtoCPoint(this->moEnd, end);
+    this->meType = LINE;
+    this->mfStep = 1;
+}
+
+void CMovePath::setCurve(const QPoint &start, const QPoint &end)
+{
+    QtoCPoint(this->moStartPoint, start);
+    QtoCPoint(this->moEnd, end);
+    this->meType = CURVE;
+    this->mfStep = 1;
+}
+
+void CMovePath::setTrigon(const QPoint &start, const QPoint &p1, const QPoint &p2)
+{
+    QtoCPoint(this->moStartPoint, start);
+    QtoCPoint(this->moPointB, p1);
+    QtoCPoint(this->moPointC, p2);
+    this->meType = TRIGON;
+    this->mfStep = 1;
+}
+
+void CMovePath::setRect(const QPoint &start, const QPoint &rect)
+{
+    QtoCPoint(this->moStartPoint, start);
+    QtoCPoint(this->moRect, rect);
+    this->meType = RECT;
+    this->mfStep = 1;
+}
+
+void CMovePath::setCentre(const QPoint &start, const QPoint &centre)
+{
+    QtoCPoint(this->moStartPoint, start);
+    QtoCPoint(this->moCentre, centre);
+    this->meType = ARC;
+    this->mfStep = 1;
+}
+
+void CMovePath::setEllipse(const QPoint &start, const QPoint &leftCentre, const QPoint &rightCentre)
+{
+    QtoCPoint(this->moStartPoint, start);
+    QtoCPoint(this->moLeftCentre, leftCentre);
+    QtoCPoint(this->moRightCentre, rightCentre);
+    this->meType = ELLIPSE;
+    this->mfStep = 1;
+}
+
+CMovePath::~CMovePath()
+{
+
+}
+
+QPoint CMovePath::getPoint(qreal step, bool *success)
+{
+    if (step < 0 || step > 1)
+    {
+        if (success)
+        {
+            *success = false;
+            return QPoint();
+        }
+    }
+    else
+    {
+        if (success)
+        {
+            *success = true;
+        }
+    }
+    qreal x = 0;
+    qreal y = 0;
+    step *= mfStep;
+    switch (meType)
+    {
+    case LINE: // 直线
+        x = (moEnd.x - moStartPoint.x) * step;
+        y = (moEnd.y - moStartPoint.y) * step;
+        x += moStartPoint.x;
+        y += moStartPoint.y;
+        break;
+    case CURVE:    // 默认实现(物理)抛物线，可以重载实现其他曲线
+    {
+        qreal height = moEnd.y - moStartPoint.y;
+        qreal width  = moEnd.x - moStartPoint.x;
+        qreal g = width * width / height;
+        x = (moEnd.x - moStartPoint.x) * step;
+        y = x * x / g;
+        x += moStartPoint.x;
+        y += moStartPoint.y;
+        break;
+    }
+    case TRIGON:    // 三角形
+    {
+        qreal AB = lengthToPoint(moStartPoint, moPointB);
+        qreal BC = lengthToPoint(moPointB, moPointC);
+        qreal CA = lengthToPoint(moPointC, moStartPoint);
+        qreal perimeter = AB + BC + CA;
+        qreal length = step * perimeter;
+        // 运动轨迹ABC
+        if (length <= AB)
+        {
+            x = (moPointB.x - moStartPoint.x) * step / AB * perimeter;
+            y = (moPointB.y - moStartPoint.y) * step / AB * perimeter;
+            x += moStartPoint.x;
+            y += moStartPoint.y;
+        }
+        else if (length <= AB + BC)
+        {
+            x = (moPointC.x - moPointB.x) * (step - AB / perimeter) / BC * perimeter;
+            y = (moPointC.y - moPointB.y) * (step - AB / perimeter) / BC * perimeter;
+            x += moPointB.x;
+            y += moPointB.y;
+        }
+        else
+        {
+            x = (moStartPoint.x - moPointC.x) * (step - (AB + BC) / perimeter) / CA * perimeter;
+            y = (moStartPoint.y - moPointC.y) * (step - (AB + BC) / perimeter) / CA * perimeter;
+            x += moPointC.x;
+            y += moPointC.y;
+        }
+        break;
+    }
+    case RECT: // 矩形
+    {
+        CPoint A = moStartPoint;
+        CPoint B = moEnd;
+        CPoint C = CPoint{B.x, A.y};
+        CPoint D = CPoint{A.x, B.y};
+
+        qreal AC = lengthToPoint(A, C);
+        qreal AD = lengthToPoint(A, D);
+        qreal CB = lengthToPoint(C, B);
+        qreal DB = lengthToPoint(B, D);
+        qreal perimeter = AC + AD + CB + DB;
+        qreal length = step * perimeter;
+
+        // 运动轨迹ACBD
+        if (length <= AC)
+        {
+            y = A.y;
+            x = A.x + (C.x - A.x) * step / AC * perimeter;
+        }
+        else if (length <= AC + CB)
+        {
+            x = C.x;
+            y = C.y + (B.y - C.y) * (step - AC / perimeter) / CB * perimeter;
+        }
+        else if (length <= AC + CB + DB)
+        {
+            y = B.y;
+            x = B.x + (A.x - C.x) * (step - 0.5) / DB * perimeter;
+        }
+        else
+        {
+            x = D.x;
+            y = D.y + (A.y - D.y) * (step - 0.5 - DB / perimeter) / AD * perimeter;
+        }
+        break;
+    }
+    case ELLIPSE: // 还未完整实现
+    {
+        qreal a = lengthToPoint(moLeftCentre, moRightCentre);
+        qreal b = lengthToPoint(moLeftCentre, moStartPoint);
+        qreal c = lengthToPoint(moRightCentre, moStartPoint);
+        qreal L = b + c;
+#define COS(a, b, c) (((a)*(a) + (b)*(b) - (c)*(c))/2/(a)/(b))
+
+        qreal C = COS(a, b, c);
+        qreal m = step * mfStep * 360;
+
+        qreal xl = (a*a - L*L) / (qCos(C)*a - 2*L);
+
+
+        x = (qSin(C-m) * xl) / qSin(C) / b * moStartPoint.x;
+        y = (qCos(C-m) * xl) / qCos(C) / b * moStartPoint.y;
+        break;
+    }
+    case ARC: // 圆形
+    {
+        // 圆半径
+        qreal radius = lengthToPoint(moCentre, moStartPoint);
+        int bL = moCentre.x > moStartPoint.x ? -1:1;
+        // 起始位置斜率
+        if (moCentre.x == moStartPoint.x)
+        {
+            moStartPoint.x += 1;
+        }
+
+        qreal startAngle = qAtan((moCentre.y - moStartPoint.y)/(moCentre.x - moStartPoint.x)) * 180 / M_PI;
+
+        // 旋转角度
+        qreal angleA = step * mfStep * 360;
+        // 半径的平方
+        qreal radius2 = radius * radius;
+
+        // 旋转后直线斜率
+        qreal k;
+        // 旋转后和圆的交点坐标（两个交点）
+        qreal x1;
+        qreal x2;
+        qreal y1;
+        qreal y2;
+        if (qAbs(startAngle + angleA) != 90 || qAbs(startAngle + angleA) != 270)
+        {
+            k = qTan((startAngle + angleA) / 180 * M_PI);
+            x1 = bL * qSqrt(radius2 / (1 + k * k)) + moCentre.x;
+            x2 = bL * -qSqrt(radius2 / (1 + k * k)) + moCentre.x;
+
+            y1 = k * (x1 - moCentre.x) + moCentre.y;
+            y2 = k * (x2 - moCentre.x) + moCentre.y;
+        }
+        else
+        {
+            x1 = x2 = moCentre.x;
+            y1 = moCentre.y + radius;
+            y2 = moCentre.y - radius;
+        }
+
+        if (startAngle + angleA <= 90)
+        {
+            x = x1;
+            y = y1;
+        }
+        else if (startAngle + angleA <= 180)
+        {
+            x = x2;
+            y = y2;
+        }
+        else if (startAngle + angleA <= 270)
+        {
+            x = x2;
+            y = y2;
+        }
+        else
+        {
+            x = x1;
+            y = y1;
+        }
+        break;
+    }
+    default:
+        break;
+    }
+    return QPoint(x, y);
+}
+
+qreal CMovePath::lengthToPoint(const CPoint &p1, const CPoint &p2)
+{
+    qreal x = qAbs(p1.x - p2.x);
+    qreal y = qAbs(p1.y - p2.y);
+    return qSqrt((x*x) + (y*y));
+}
+
+qreal CMovePath::lengthToPoint(const QPoint &p1, const QPoint &p2)
+{
+    qreal x = qAbs(p1.x() - p2.x());
+    qreal y = qAbs(p1.y() - p2.y());
+    return qSqrt((x*x) + (y*y));
+}
---
+++ cmovepath.h
@@ -0,0 +1,68 @@
+﻿#ifndef CMOVEPATH_H
+#define CMOVEPATH_H
+
+#include <QPoint>
+#include <QRectF>
+
+struct CPoint {
+    qreal x;
+    qreal y;
+};
+
+class CShapeCircle
+{
+public:
+    CShapeCircle(const QPoint &centre, qreal radius);
+    CShapeCircle(const QPoint &centre, const QPoint &point);
+
+    QList<QPoint> getPoint(qreal x, bool isX = true);
+
+private:
+    qreal mfRadius;
+    QPoint moCentre;
+};
+
+class CMovePath
+{
+public:
+    enum TYPE {LINE, CURVE, TRIGON, RECT, ELLIPSE, ARC, UDF};
+    CMovePath(TYPE type = UDF);
+    CMovePath(const QPoint &start, const QPoint &moEnd, TYPE meType = LINE);
+    CMovePath(const QPoint &start, const QPoint &moLeftCentre, const QPoint &moRightCentre);
+    void QtoCPoint(CPoint &p1, const QPoint &p2);
+    void setType(TYPE type);
+    void setSetp(qreal setp);
+    void setLine(const QPoint &start, const QPoint &end);
+    void setCurve(const QPoint &start, const QPoint &end);
+    void setTrigon(const QPoint &start, const QPoint &p1, const QPoint &p2);
+    void setRect(const QPoint &start, const QPoint &rect);
+    void setCentre(const QPoint &start, const QPoint &moCentre);
+    void setEllipse(const QPoint &start, const QPoint &moLeftCentre, const QPoint &moRightCentre);
+    ~CMovePath();
+
+    virtual QPoint getPoint(qreal step, bool *success = NULL); // step must between 0 and 1.
+
+    static qreal lengthToPoint(const CPoint &p1, const CPoint &p2);
+    static qreal lengthToPoint(const QPoint &p1, const QPoint &p2);
+protected:
+    qreal  mfStep;
+    CPoint moStartPoint;
+    TYPE   meType;
+
+private:
+    union{
+        CPoint moEnd;    // 线段
+        CPoint moCentre; // 圆
+        CPoint moRect;   // 矩形
+        struct {       // 三角形
+            CPoint moPointB;
+            CPoint moPointC;
+        };
+        struct {       // 椭圆
+            CPoint moLeftCentre;
+            CPoint moRightCentre;
+        };
+    };
+};
+
+#endif // CMOVEPATH_H
